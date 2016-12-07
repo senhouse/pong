6c3379f324fd9e02db6cc9f0711f4f2225aa0a24
Scott Senhouse <scottsenhouse@gmail.com>
2016-12-06 20:04:50 -0500

    I already followed tutorial plus added trail, sound, and working on score

---
+++ Assets/PongSounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongSounds : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.collider.name == "Ball"){
+			GetComponent<AudioSource>().Play();
+		}
+
+	}
+}
