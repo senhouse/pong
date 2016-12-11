688ecd78c3b5888d281fd03860fafec97d8e7478
Scott Senhouse <scottsenhouse@gmail.com>
2016-12-10 19:42:09 -0500

    Pause button pauses. working on menu animation now

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -9,13 +9,24 @@ public class UI : MonoBehaviour {
 			anim.playAutomatically = false;*/
 	}
 	
+	void OnMouseDown()
+     {
+         if(gameObject.tag == "PauseButton"){
+             if(Time.timeScale == 1.0f)
+            {
+                Time.timeScale = 0;
+                
+            } else if (Time.timeScale == 0){
+                Time.timeScale = 1;
+                
+            }
+          }
+     }
+
 	// Update is called once per frame
 	void Update () {
 			/*anim = GetComponent<Animation>();
 			anim.playAutomatically = false;*/
-			//if (Input.GetKeyDown("p")) {
-				//GetComponent<Animation>().SetTrigger("Pause");
-
-			//}
+			
 	}
 }
